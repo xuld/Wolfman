6c2823fac2348d344c544a9ad9a91f07cf5d6ec5
Sadamodou <305965707@qq.com>
2016-08-08 13:46:35 +0800

    完成狼人和女巫的功能

--- src/gameFunction.ts
+++ src/gameFunction.ts
@@ -19,9 +19,35 @@ class DistributionRoles {
 
 }
 
+class WolfMan extends DistributionRoles {
+
+    roles = Roles.wolfman;
+
+    alive = 1;
+
+}
+
+class Villagers extends DistributionRoles {
+
+    roles = Roles.villagers;
+
+    alive = 1;
+
+}
+
+class Hunter extends DistributionRoles {
+
+    roles = Roles.hunter;
+
+    alive = 1;
+
+}
+
 class Witch extends DistributionRoles {
 
     roles = Roles.witch;
+
+    alive = 1;
 
     poison = 1;
 
@@ -30,30 +56,54 @@ class Witch extends DistributionRoles {
 }
 
 /*天黑*/
-class Dark {
+class Night {
+
+    player: DistributionRoles[] = [];
+
+    add(item: DistributionRoles) {
+        this.player.push(item);
+    }
 
     wolfmans: DistributionRoles[] = [];
 
-    addWolfMan(item: DistributionRoles) {
-        if (item.roles === Roles.wolfman) {
-            this.wolfmans.push(item);
+    /*将狼人分为一组*/
+    addWolfMan() {
+        for (let i = 0; i < this.player.length; i++) {
+            if (this.player[i].roles === Roles.wolfman) {
+                this.wolfmans.push(this.player[i]);
+            }
         }
     }
 
-    wolfManKill() {
+    wolfManKill(index:number) {
         document.write("请指定杀人目标!");
+        this.player[index].alive = 0;
     }
 
-    witch(item: Witch) {
+    witch(item: Witch,index:number) {
         if (item.alive == 1) {
             document.write("是否使用解药?");
             if (true && item.poison == 1) {
-                救一个被杀的人
+                /*救一个被杀的人*/
+                let live: DistributionRoles[] = [];
+                for (let i = 0; i < this.player.length; i++){
+                    if (this.player[i].alive === 0) {
+                        live.push(this.player[i]);
+                    }
+                }
+                live[index].alive = 1;
                 item.poison = 0;
             }
             document.write("是否使用毒药？");
             if (true && item.antidote == 1) {
-                杀死一个人
+                /*杀死一个人*/
+                let dead: DistributionRoles[] = [];
+                for (let i = 0; i < this.player.length; i++) {
+                    if (this.player[i].alive === 1) {
+                        dead.push(this.player[i]);
+                    }
+                }
+                dead[index].alive = 0;
                 item.antidote = 0;
             }
         }
